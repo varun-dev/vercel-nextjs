f4272de0f34843067762198d30a1945ede82c8dd
Varun Dev <varun.dev@hotmail.com>
2022-02-09 15:13:54 +0100

    multilingual support

--- utils/list-utils.js
+++ utils/list-utils.js
@@ -5,4 +5,17 @@
  * Importing code from github gist
  * https://gist.github.com/varun-dev/8d4254c2dbc45e2d5c9d336ae367472a
  */
-export * from 'https://gist.githubusercontent.com/varun-dev/8d4254c2dbc45e2d5c9d336ae367472a/raw/11d1819a9f7bc9885f04ee7ba15b88b4421f8872/list-utils.js'
+export {
+  _append,
+  _filterAndMap,
+  _removeBy,
+  _updateBy,
+  _eqProp,
+  _prop,
+} from 'https://gist.githubusercontent.com/varun-dev/8d4254c2dbc45e2d5c9d336ae367472a/raw/11d1819a9f7bc9885f04ee7ba15b88b4421f8872/list-utils.js'
+
+export const _keyValues = (list, key, valueKey) =>
+  list.reduce((resp, item) => {
+    resp[item[key]] = item[valueKey]
+    return resp
+  }, {})
